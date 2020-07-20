57bedfefa43a486ff1b4e74077c56417bb427642
Eliud Kagema <eliud.k.maina@gmail.com>
2020-07-20 12:14:25 +0300

    adding templates and routes

--- application/routes.py
+++ application/routes.py
@@ -2,5 +2,27 @@ from application import app
 from flask import render_template
 
 @app.route("/")
-def hello():
-    return render_template('index.html')
+@app.route("/index")
+def index():
+    return render_template('index.html', login=False)
+
+
+@app.route("/courses")
+def courses():
+    return render_template('index.html', login=False)
+
+
+@app.route("/register")
+def register():
+    return render_template('index.html', login=False)
+
+
+@app.route("/login")
+def login():
+    return render_template('index.html', login=False)
+
+
+
+@app.route("/enrollment")
+def enrollment():
+    return render_template('index.html', login=False)
