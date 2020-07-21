571724a181a83f06aa92fc2b6a2f80a2f7717491
Eliud Kagema <eliud.k.maina@gmail.com>
2020-07-21 15:55:49 +0300

    working

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,13 @@
 from application import app
 from flask import render_template, request
 
+courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, 
+              {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, 
+              {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, 
+              {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, 
+              {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+
+
 @app.route("/")
 @app.route("/index")
 def index():
@@ -10,8 +17,6 @@ def index():
 @app.route("/courses/")
 @app.route("/courses/<term>")
 def courses(term=2019):
-    courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
-    print(courseData[0]["title"])
     return render_template("courses.html", courseData=courseData, courses = True )
 
 
@@ -26,14 +31,18 @@ def login():
 
 
 
-@app.route("/enrollment")
+@app.route("/enrollment", methods=["GET","POST"])
 def enrollment():
-    id = request.args.get('courseID')
-    title = request.args.get('title')
-    term = request.args.get('term')
-    context = {
-        'id': id,
-        'term': term,
-        'title': title,
-    }
-    return render_template('enrollment.html', context, enrollment=True)
+    id = request.form.get('courseID')
+    title = request.form['title']
+    term = request.form.get('term')
+    return render_template("enrollment.html", enrollment=True, data={"id":id,"title":title,"term":term}) 
+
+
+@app.route('/api/') 
+@app.route('/api/<idx>') 
+def api(idx=None):
+    if(idx==None):
+        jdata = courseData
+    else:
+        jdata = courseData[idx]
